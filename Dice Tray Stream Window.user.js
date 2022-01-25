f0a58a115dc80d542499429e1d7cfcc9e2765ef0
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-01-24 23:13:27 -0500

    Add files via upload

---
+++ Dice Tray Stream Window.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Dice Tray Stream Window
+// @namespace    Azmoria
+// @version      0.1
+// @description  Stream your Dice to another window
+// @author       Azmoria
+// @include      https://www.dndbeyond.com/profile/*/characters/*
+// @include      https://www.dndbeyond.com/characters/*
+// @include      https://www.dndbeyond.com/encounter-builder
+// @include      https://www.dndbeyond.com/combat-tracker*
+// @include      https://www.dndbeyond.com/monsters/*
+// @include      https://www.dndbeyond.com/campaigns/*
+// @include      https://www.dndbeyond.com/encounters/*
+// @include      https://www.dndbeyond.com/my-encounters
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dndbeyond.com
+// ==/UserScript==
+
+window.onload = function(){
+
+const childWindow = window.open('DiceTray')
+childWindow.document.write('<video id="video"></video>')
+const canvas = document.querySelector('canvas');
+const video = childWindow.document.querySelector('video');
+const body = childWindow.document.querySelector('body');
+video.setAttribute("controls", '');
+body.setAttribute("id", 'diceTrayBody');
+const stream = canvas.captureStream();
+video.srcObject = stream;
+
+
+}
+
+
+
+
+
