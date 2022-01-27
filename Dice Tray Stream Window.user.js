7e805879779da54e895c762d36acc68ffae043d6
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-01-27 14:59:34 -0500

    Update Dice Tray Stream Window.user.js

--- Dice Tray Stream Window.user.js
+++ Dice Tray Stream Window.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Dice Tray Stream Window
 // @namespace    Azmoria
-// @version      0.8
+// @version      0.9
 // @description  Stream your Dice to another window
 // @author       Azmoria
 // @downloadURL  https://github.com/Azmoria/dndbeyonddark/raw/master/Dice%20Tray%20Stream%20Window.user.js
 // @updateURL    https://github.com/Azmoria/dndbeyonddark/raw/master/Dice%20Tray%20Stream%20Window.user.js
 // @include      https://www.dndbeyond.com/profile/*/characters/*
-// @include      https://www.dndbeyond.com/characters/*
 // @include      https://www.dndbeyond.com/encounter-builder
 // @include      https://www.dndbeyond.com/combat-tracker*
 // @include      https://www.dndbeyond.com/encounters/*
