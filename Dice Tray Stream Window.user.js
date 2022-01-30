b0b742a0bbf312a8f0d39d9fbb4f4951263f6feb
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-01-29 21:21:41 -0500

    Update Dice Tray Stream Window.user.js

--- Dice Tray Stream Window.user.js
+++ Dice Tray Stream Window.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dice Tray Stream Window
 // @namespace    Azmoria
-// @version      0.9
+// @version      1.0.001
 // @description  Stream your Dice to another window
 // @author       Azmoria
 // @downloadURL  https://github.com/Azmoria/dndbeyonddark/raw/master/Dice%20Tray%20Stream%20Window.user.js
@@ -13,19 +13,53 @@
 // @include      https://www.dndbeyond.com/my-encounters
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dndbeyond.com
 // ==/UserScript==
+var childWindow;
+
+function OpenWindow()
+{
+
+  if(typeof(childWindow) == 'undefined' || childWindow.closed)
+  {
+    //create new
+    var url = "https://www.dndbeyond.com/DiceTray";
+    childWindow = window.open('', 'Dice Tray');
+    var childUrl = childWindow.document.location.href;
+    var subChildStr = childUrl.substring(childUrl.length - 9)
+    if(childWindow == null || subChildStr != "#DiceTray")
+    {
+      childWindow = window.open(url, 'Dice Tray');
+    }
+  }
+  else
+  {
+
+  }
+}
 
 setTimeout(function() {
-    const childWindow = window.open("", "");
-    childWindow.document.write('<video id="video" muted autoplay></video>');
+
+    OpenWindow();
+    if(childWindow.document.querySelector('video')==null){
+        childWindow.document.write('<video id="video" muted autoplay></video>');
+    }
     const video = childWindow.document.querySelector('video');
     const body = childWindow.document.querySelector('body');
     const canvas = document.querySelector('canvas');
-    const stream = canvas.captureStream();
+    var stream = canvas.captureStream(60);
     body.setAttribute("id", 'diceTrayBody');
-    video.srcObject = stream;
-    childWindow.history.pushState({}, "Dice Tray - " + document.title, "#DiceTray");
+    childWindow.postMessage("addSourceToVideo", childWindow.document.location.href);
     childWindow.document.title = "Dice Tray - " + document.title;
+    childWindow.history.pushState({}, "Dice Tray - " + document.title, "/" + document.title +"#DiceTray");
+    function addSourceToVideo(element, src) {
+       element.srcObject = src;
+    }
+
+    childWindow.addEventListener("message", function (event) {
+           addSourceToVideo(video, stream);
+    }, false);
+
 }, 1000);
+
 
 
 
